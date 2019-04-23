8e2eef25c61ffcfe9a26e8030cbb4ae55f18b228
Kien <kien@skydisc.jp>
2019-04-24 08:39:24 +0900

    Add load data from folder method

--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,6 @@
 from scipy.io import loadmat
 import pandas as pd
+from glob import glob
 
 def load_data(file_path):
     """Read data from path and return data array.
@@ -23,3 +24,44 @@ def load_data(file_path):
     
     data = mat['X%s_DE_time'%index]
     return data
+
+def load_data_from_folder(folder_path):
+    """Read data from folder and return pandas DataFrame.
+
+    Parameters:
+    ----------
+    folder_path: string
+        Path to folder contains files without trailing '/'
+
+    Returns:
+    data: array
+        Array contains data
+
+    """
+    if folder_path.endswith('/'):
+        folder_path+='*'
+    else:
+        folder_path+='/*'
+    files = glob(folder_path)
+    files.sort()
+    
+    arr = []
+    for f in files:
+        data = load_data(f)
+        
+        tmp = f.split('/')
+        index = str(tmp[len(tmp)-1].split('.')[0])
+        
+        lb = tmp[len(tmp)-2]
+        if lb=='OK':
+            label = 'normal'
+            label_n = 0
+        else:
+            label = 'abnormal'
+            label_n = 1
+            
+        arr.append([index, data, label, label_n])
+        
+    df = pd.DataFrame(arr)
+    df.columns = ['id', 'data', 'label', 'label_number']
+    return df
