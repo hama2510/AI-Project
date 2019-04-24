31266275a1501d153e11822cb15f94a390fe5c78
Ubuntu <ubuntu@ip-172-31-58-210.ec2.internal>
2019-04-24 02:53:11 +0000

    Add activation

--- utils/cnn_layers.py
+++ utils/cnn_layers.py
@@ -1,8 +1,8 @@
 import keras.backend as K
 from keras.layers import Conv2DTranspose, Lambda
 
-def Conv1DTranspose(input_tensor, filters, kernel_size, strides=1, padding='valid'):
+def Conv1DTranspose(input_tensor, filters, kernel_size, strides=1, padding='valid', activation='linear'):
     x = Lambda(lambda x: K.expand_dims(x, axis=2))(input_tensor)
-    x = Conv2DTranspose(filters=filters, kernel_size=(kernel_size, 1), strides=(strides, 1), padding=padding)(x)
+    x = Conv2DTranspose(filters=filters, kernel_size=(kernel_size, 1), strides=(strides, 1), padding=padding, activation=activation)(x)
     x = Lambda(lambda x: K.squeeze(x, axis=2))(x)
     return x
