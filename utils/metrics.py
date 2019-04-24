0b2e96c6a519a6e39362c94dc5e78db71ce7856f
Ubuntu <ubuntu@ip-172-31-58-210.ec2.internal>
2019-04-24 02:41:35 +0000

    Add evaluation metrics

--- utils/metrics.py
+++ utils/metrics.py
@@ -7,7 +7,7 @@ def mse(y_true, y_pred):
     return np.mean(np.power((y_true-y_pred), 2), axis=(1,2))
 
 def rmse(y_true, y_pred):
-    return np.sqrt(np.mean(np.square(y_pred - y_true), axis=(1,2)) 
+    return np.sqrt(np.mean(np.square(y_pred - y_true), axis=(1,2))) 
 
 def smape(y_true, y_pred):
     return np.mean(2.0*abs(y_true - y_pred)/(abs(y_true) + abs(y_pred)), axis=(1,2))
